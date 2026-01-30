4d136d899b2152e6ef3904372f8ec323d79b18a3
fliker09 <fliker09@users.noreply.github.com>
2026-01-31 00:24:16 +0200

    Update 04_sony_continuous_bracketing_0.3EV_ARW_V2.sh and 08_sony_continuous_bracketing_0.3EV_JPG_V2.sh

--- sony_mark_III/04_sony_continuous_bracketing_0.3EV_ARW_V2.sh
+++ sony_mark_III/04_sony_continuous_bracketing_0.3EV_ARW_V2.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
+# Alexandru Barbovschi (c) 2025-2026
+# Uncomment the line below to show the execution of the script in full detail.
+#set -x
+# Uncomment the line below to execute the script step by step (except for functions).
+#trap read debug
 
+# This script was a wild experiment - what if we launched gphoto2 only once and run all the comands
+# through that single instance? Yes, that is possible, as gphoto2 does support shell mode!
+# To interact with that shell, 'tmux' was chosen as intermediary ('screen' proved to be too slow).
+# The experiment turned out to be a success, it's the fastest among all the scripts!
+
+# We need a delay for gphoto2's shell reading.
 DELAY=0.1
-TIMESTAMP=$(date +%s)
+# Provide an unique suffix for the launched instance, based on the epoch time in microseconds.
+TIMESTAMP=$(date +%s%3N)
 
+
+# This function checks the currently set shutter speed and if it matches the requested one.
+# If it doesn't - it tries to set it to the requested value in 10 attempts at most.
+# This itteration is more complex due to the use of gphoto2's shell and tmux to interact with it.
 function check_shutter_speed() {
+    # Uncomment the line below to execute the function step by step
+    #trap read debug
+    # If you did uncomment the line above - uncomment the last line as well,
+    # to limit this mode to this function alone!
     wait_for_prompt
     for j in $(seq 1 10)
     do
@@ -19,9 +39,17 @@ function check_shutter_speed() {
             break
         fi
     done
+    #trap - debug
 }
 
+# This function is basically checking for new output line in gphoto2's shell.
+# It's time constrained through the use of 'for' cycle. The value of 1000 was
+# not chosen for any specific reason and probably should be reduced.
 function wait_for_prompt() {
+    # Uncomment the line below to execute the function step by step
+    #trap read debug
+    # If you did uncomment the line above - uncomment the last line as well,
+    # to limit this mode to this function alone!
     for i in $(seq 1 1000)
     do
         last_line=$(tail -1 /dev/shm/gphoto2_"$TIMESTAMP".log | grep -E '/> $')
@@ -32,9 +60,17 @@ function wait_for_prompt() {
             break
         fi
     done
+    #trap - debug
 }
 
 
+echo "!!! This script is assuming that you've already set 'File Format' to 'RAW' !!!"
+echo
+
+# An attempt to accelerate ever so slightly the gphoto2 commands,
+# by making it not search for the available cameras. This piece of code
+# just searches for the camera and stores its model name and USB port,
+# so it can be re-used later in the script.
 while IFS= read -r line; do
     # Trim leading/trailing whitespace
     line="$(echo "$line" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
@@ -55,14 +91,18 @@ while IFS= read -r line; do
 done < <(gphoto2 --auto-detect)
 echo
 
+# Start a gphoto2 shell in a tmux session and then make it output into a log file.
 tmux new-session -s gphoto2_"$TIMESTAMP" -d 'gphoto2 --camera="'"$model"'" --port="'"$port"'" --filename %Y_%m_%d_%H_%M_%S_%n.ARW --shell'
 tmux pipe-pane -t gphoto2_"$TIMESTAMP":0.0 -o "cat > /dev/shm/gphoto2_$TIMESTAMP.log"
 
-tmux send-keys -t gphoto2_"$TIMESTAMP" 'set-config /main/capturesettings/capturemode=Bracketing C 3.0 Steps 5 Pictures' C-m 'set-config /main/capturesettings/shutterspeed=1/80' C-m 'set-config /main/capturesettings/imagequality=RAW' C-m 'set-config /main/actions/bulb=0' C-m
-
+# Set the starting shutter speed and Bracketing Continuous mode.
+# This may take a while, depending on what settings you had before starting this script.
+# For easier and consistent benchamarking please use prepare_usb.sh script!
+tmux send-keys -t gphoto2_"$TIMESTAMP" 'set-config /main/capturesettings/capturemode=Bracketing C 3.0 Steps 5 Pictures' C-m 'set-config /main/capturesettings/shutterspeed=1/80' C-m 'set-config /main/actions/bulb=0' C-m
+# Ensure that Shutter Speed was indeed set at the desired value, as it's not guaranteed in one try.
 check_shutter_speed 1/80
 echo
-
+# Ensure that Bracketing Continuous mode was properly set as well.
 for l in $(seq 1 10)
 do
     tmux send-keys -t gphoto2_"$TIMESTAMP" 'get-config /main/capturesettings/capturemode' Enter
@@ -79,6 +119,10 @@ do
 done
 echo
 
+# We can emulate keypress into tmux's session. Through this we are sending commands
+# into gphoto2's shell. As we can see - we need to send bulb commands in a way
+# which basically emulates real life pressing on shutter release button. Neat, right?!
+# In a similar way we order it to adjust shutter speed for the next sequence.
 tmux send-keys -t gphoto2_"$TIMESTAMP" 'set-config /main/actions/bulb=1' Enter
 sleep 1
 tmux send-keys -t gphoto2_"$TIMESTAMP" 'set-config /main/actions/bulb=0' Enter
@@ -93,8 +137,14 @@ tmux send-keys -t gphoto2_"$TIMESTAMP" 'set-config /main/capturesettings/shutter
 check_shutter_speed 1/20
 echo
 
+# Here we "click" on the proverbial shutter release button and... keep it that way, for now.
 tmux send-keys -t gphoto2_"$TIMESTAMP" 'set-config /main/actions/bulb=1' Enter
 
+# Thanks to the command above, the sequence which includes the 3.2s exposure is underway.
+# As we already know, Sony cameras allow file dumping to happen in parallel
+# to shooting, so we will take advantage of it and start exactly that!
+# Note: this can actually be used to benchmark file transfer speed, as there is no
+# time wasted on initialiation and stuff. This should be pretty accurate approach!
 echo "Dumping photos..."
 arw_amount=0
 time while [ "$arw_amount" != 15 ]
@@ -107,6 +157,7 @@ done
 echo
 echo "Photos dumped, stopping gphoto2!"
 echo
+# Once the shots are dumped, we "unclick" the shutter release button and
+# order gphoto2 shell to close up. After that we delete the log file.
 tmux send-keys -t gphoto2_"$TIMESTAMP" 'set-config /main/actions/bulb=0' C-m 'exit' C-m
-
 rm /dev/shm/gphoto2_$TIMESTAMP.log
